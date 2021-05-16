4ec778b8b53bc9b06a0badf6df159e1051ee5b87
201402407 <banner4@naver.com>
2021-05-17 01:18:42 +0900

    RVO 수정

--- src/services/main/restSample.ts
+++ src/services/main/restSample.ts
@@ -2,33 +2,33 @@ import { HttpService } from "@/utils/http"
 import { AxiosResponse } from "axios"
 
 interface Status {
-    code: string
-    codeMsg: string
-    message: string
+  code: string
+  codeMsg: string
+  message: string
 }
 export interface LoginPVO {
-    emailId: string
-    pwd: string
+  emailId: string
+  pwd: string
 }
 
 export interface LoginRVO {
-    status?: Status
-    result?: string
+  status?: Status
+  result?: string
 }
 
 export interface CountRVO {
-    status?: Status
-    count: number
+  status?: Status
+  count: number
 }
 
 // 서비스 로직 단 구현
 export default class RestSample extends HttpService {
-    // 로그인
-    async login(pvo: LoginPVO): Promise<AxiosResponse<LoginRVO>> {
-        return await this.getInstance().post("api/common/login", pvo)
-    }
+  // 로그인
+  async login(pvo: LoginPVO): Promise<AxiosResponse<LoginRVO>> {
+    return await this.getInstance().post("api/common/login", pvo)
+  }
 
-    async getCount(): Promise<AxiosResponse<CountRVO>> {
-        return await this.getInstance().get("api/getCount")
-    }
+  async getCount(): Promise<AxiosResponse<CountRVO>> {
+    return await this.getInstance().get("api/getCount")
+  }
 }
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-        <h1>{{ result }}</h1>
-        <input v-model="pvo.emailId" type="text" placeholder="ID 입력" /><br />
-        <input v-model="pwd" type="password" placeholder="PWD 입력" /><br />
-        <button @click="clickBtn">버튼클릭하래</button><br />
-        <button @click="clickBtn2">getCount</button><br />
-        <h2>{{ count }}</h2>
-    </div>
+  <div>
+    <h1>{{ result }}</h1>
+    <input v-model="pvo.emailId" type="text" placeholder="ID 입력" /><br />
+    <input v-model="pwdInput" type="password" placeholder="PWD 입력" /><br />
+    <button @click="clickBtn">버튼클릭하래</button><br />
+    <button @click="clickBtn2">getCount</button><br />
+    <h2>{{ count }}</h2>
+  </div>
 </template>
 
 <script lang="ts">
@@ -17,31 +17,31 @@ import CryptoJS from "crypto-js"
 
 @Component
 export default class Main extends Vue {
-    private testNum = 0
-    private testName = ""
-    private count = 0
-    private result = {}
-    private restSample = new RestSample()
-    private pwd = ""
-    private pvo: LoginPVO = {
-        emailId: "",
-        pwd: "",
-    }
+  private testNum = 0
+  private testName = ""
+  private count = 0
+  private pwdInput = ""
+  private result = {}
+  private restSample = new RestSample()
+  private pvo: LoginPVO = {
+    emailId: "",
+    pwd: "",
+  }
 
-    async clickBtn() {
-        this.pvo.pwd = this.hashing(this.pwd)
-        const rvo = await MainModule.chkLogin(this.pvo)
-        console.log(rvo)
-    }
+  async clickBtn() {
+    this.pvo.pwd = this.hashing(this.pwdInput)
+    const rvo = await MainModule.chkLogin(this.pvo)
+    console.log(rvo)
+  }
 
-    private hashing(str: string) {
-        return CryptoJS.SHA256(str).toString()
-    }
+  private hashing(str: string) {
+    return CryptoJS.SHA256(str).toString()
+  }
 
-    async clickBtn2() {
-        const rvo = await MainModule.getCount()
-        console.log(rvo)
-        this.count = rvo.data.count
-    }
+  async clickBtn2() {
+    const rvo = await MainModule.getCount()
+    console.log(rvo)
+    this.count = rvo.data.count
+  }
 }
 </script>
